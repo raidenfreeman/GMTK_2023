279ba764de811e54ce6a83815442dc445b688497
Konstantinos Gkinis <raidenfreeman@hotmail.com>
2023-07-08 19:00:35 +0200

    Add interceptor group oscillation

--- Assets/Scripts/Interceptor.cs
+++ Assets/Scripts/Interceptor.cs
@@ -6,10 +6,31 @@ public class Interceptor : MonoBehaviour
 
     [SerializeField] private float horizontalSpeed = 2;
 
+    [SerializeField] private float oscillationPeriod = 1f;
+    private int totalInterceptors;
+
+
+    private void Awake()
+    {
+        totalInterceptors = interceptors.Length;
+    }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.right * (Time.deltaTime * horizontalSpeed));
+        var time = Time.time;
+        for (int i = 0; i < totalInterceptors; i++)
+        {
+            var interceptor = interceptors[i];
+            var interceptorPosition = interceptor.transform.localPosition;
+            if (oscillationPeriod > 0)
+            {
+                var phase = (i % 4) * 0.5f;
+                interceptor.transform.localPosition =
+                    new Vector3(interceptorPosition.x,
+                        Mathf.Sin((time / oscillationPeriod + phase) * Mathf.PI));
+            }
+        }
     }
 }
