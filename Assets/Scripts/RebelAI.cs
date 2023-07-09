4c33727cb0499964e98594410f1e0a86eb3d1292
Konstantinos Gkinis <raidenfreeman@hotmail.com>
2023-07-09 15:00:11 +0200

    Rebel can sense enemies

--- Assets/Scripts/RebelAI.cs
+++ Assets/Scripts/RebelAI.cs
@@ -15,8 +15,27 @@ public class RebelAI : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        int layerMask = 1 << 3;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left, 20, layerMask);
+
+        // If it hits something...
+        if (hit.collider != null)
+        {
+            Debug.DrawRay(transform.position, hit.transform.position - transform.position);
+            // Calculate the distance from the surface and the "error" relative
+            // to the floating height.
+            // float distance = Mathf.Abs(hit.point.y - transform.position.y);
+            // float heightError = floatHeight - distance;
+            //
+            // // The force is proportional to the height error, but we remove a part of it
+            // // according to the object's speed.
+            // float force = liftForce * heightError - rb2D.velocity.y * damping;
+            //
+            // // Apply the force to the rigidbody.
+            // rb2D.AddForce(Vector3.up * force);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
